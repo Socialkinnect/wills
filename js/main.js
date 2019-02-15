372855691968ff840f713a52af0c37637c729101
Kashif <kashif@socialkinnect.com>
2019-02-15 18:29:20 +0530

    carousel and second section started

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+ $('.single-item').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots:true,
+    });
+ 
+ $('.single-item').on('afterChange', function(event, slick, direction){
+  console.log(direction);
+  $('.bannerText').addClass('hide').removeClass('wow fadeInRight');
+  $('.slick-current .bannerText').removeClass('hide').addClass('wow fadeInRight');
+
+  // left
+});
+});
+$(window).load(function(){
+
+	new WOW().init();
+})
